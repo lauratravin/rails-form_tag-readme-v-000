17cb101b630776c9e719945e2e815b266a26b38f
Laura Messa Travin <laura.travin@gmail.com>
2021-03-16 23:52:56 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,6 @@ class PostsController < ApplicationController
 
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
-redirect_to posts_path
+    redirect_to posts_path
   end
 end
